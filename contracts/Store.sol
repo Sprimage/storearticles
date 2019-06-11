7444686ec3b3366156887ff65e9d78ea58e1ccc7
dev1644 <soham.tomar@yahoo.com>
2019-06-11 13:37:53 +0530

    Fixed tests

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -1,14 +1,24 @@
 pragma solidity >= 0.5.0;
 
-contract DecentralizedJournal {
+contract Store {
+  
   mapping (address => uint256[]) public articlesByAuthor;
   uint256[] public articles;
   mapping (address => string) public usernames;
+
   function setUsername(string calldata username) external {
     usernames[msg.sender] = username;
   }
   function publishArticle() external {
     articles.push(block.number);
     articlesByAuthor[msg.sender].push(block.number);
+  }
+
+ function fetchLatestArticle() public view returns(uint256) {
+    uint length = articlesByAuthor[msg.sender].length - 1;
+    return articlesByAuthor[msg.sender][length];
+  }
+
+  function () external {
   }
 }
--- test/1_store_article.js
+++ test/1_store_article.js
@@ -1,37 +1,45 @@
-var Store = artifacts.require("./Store.sol");
-const assert = require('assert');
-var Tx = require('ethereumjs-tx');
-const keccak256 = require('keccak256');
-const ganache = require("ganache-cli");
+const Store = artifacts.require("./Store.sol");
 const Web3 = require("web3");
-var privateKey = Buffer.from('274ae2fd57cec8d3b260c988a432c958593d4b965704cee17b4a564b85c4187c', 'hex');
 
 let contractInstance;
-const web3 = new Web3(ganache.provider());
+let options = {
+  defaultBlock: "latest",
+  transactionConfirmationBlocks: 0,
+  transactionBlockTimeout: 0
+};
+const web3 = new Web3("http://localhost:8545", null, options);
 
-contract('Store', (accounts) => {
-   beforeEach(async () => {
-      contractInstance = await Store.deployed()
-   })
- 
-  it('should submit an article to the blockchain', async () => {
-         var rawTx = {
-         nonce: '0x00',
-         gasPrice: '0x09184e72a000', 
-         gasLimit: '0x2710',
-         to: '0x0000000000000000000000000000000000000000', 
-         value: '0x00', 
-         data: data = keccak256(contractInstance.publishArticle).slice(12) + "lorem ipum got some articles"
-        }
-        var tx = new Tx(rawTx);
-        tx.sign(privateKey);
-        var serializedTx = tx.serialize();
-        //console.log(serializedTx.toString('hex'));
-        //f889808609184e72a00082271094000000000000000000000000000000000000000080a47f74657374320000000000000000000000000000000000000000000000000000006000571ca08a8bbf888cfa37bbf0bb965423625641fc956967b81d12e23709cead01446075a01ce999b56a8a88504be365442ea61239198e23d1fce7d00fcfc5cd3b44b7215f
-        web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
-         if (!err)
-           console.log(hash); // "0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385"
-        });
-   })
-})
-     
+contract("Store", accounts => {
+  beforeEach(async () => {
+    contractInstance = await Store.deployed();
+  });
+
+  it("should submit an article to the blockchain", async () => {
+    var rawTx = {
+      gasPrice: web3.utils.numberToHex("100000000000"),
+      gasLimit: web3.utils.numberToHex("1500000"),
+      from: accounts[0],
+      to: contractInstance.address,
+      value: "0x00",
+      data: web3.utils
+        .keccak256("publishArticle()")
+        .slice(0, 10)
+        .concat(web3.utils.utf8ToHex("lorem ipum got some articles").slice(2))
+    };
+
+    const txHash = await web3.eth.sendTransaction(rawTx);
+  });
+
+  it("should fetch article fome the blockchain", async () => {
+    const number = await contractInstance.fetchLatestArticle({
+      from: accounts[0],
+      gas: 50000
+    });
+    const transactionHash = (await web3.eth.getBlock(number)).transactions[0];
+    const transactionDetails = await web3.eth.getTransaction(transactionHash);
+    const article = web3.utils.toUtf8(
+      "0x" + transactionDetails.input.slice(10)
+    );
+    assert.equal(article, "lorem ipum got some articles");
+  });
+});
